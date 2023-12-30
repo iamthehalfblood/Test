5f63b1f68a77595abe740f7ce8d4444e506aeefc
iamthehalfblood <134286454+iamthehalfblood@users.noreply.github.com>
2023-12-30 15:59:23 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,13 @@
+// questions.js
+const questions = [
+  { question: 'What is the capital of France?', answer: 'Paris', rejectMessage: 'Oops! Wrong answer for the first question.' },
+  { question: 'What is the largest planet in our solar system?', answer: 'Jupiter', rejectMessage: 'Uh-oh! That\'s not the right answer for the second question.' },
+  { question: 'Who wrote "Romeo and Juliet"?', answer: 'Shakespeare', rejectMessage: 'Incorrect! The third question is a bit tricky.' },
+  { question: 'What is the square root of 144?', answer: '12', rejectMessage: 'Not quite right for the fourth question.' },
+  { question: 'Which element has the chemical symbol "O"?', answer: 'Oxygen', rejectMessage: 'Wrong! Try again for the fifth question.' },
+  { question: 'In which year did the Titanic sink?', answer: '1912', rejectMessage: 'Incorrect! The sixth question tests your historical knowledge.' },
+  { question: 'What is the capital of Japan?', answer: 'Tokyo', rejectMessage: 'Oops! The seventh question is about world capitals.' },
+  { question: 'Who painted the Mona Lisa?', answer: 'Da Vinci', rejectMessage: 'Incorrect! The eighth question is about art.' },
+  { question: 'What is the largest ocean on Earth?', answer: 'Pacific', rejectMessage: 'Wrong! The ninth question tests your geography.' },
+  { question: 'How many continents are there?', answer: '7', rejectMessage: 'Oops! The tenth question is about geography.' },
+];
