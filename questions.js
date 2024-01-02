195df3d73505b069545f3d8afd2d8e35f25a49b1
iamthehalfblood <134286454+iamthehalfblood@users.noreply.github.com>
2024-01-02 13:58:06 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,7 +5,7 @@ const questions = [
   { question: 'In Which city did we met for the third time?', answer: 'Bareilly', rejectMessage: 'You called me in that city and you do not know, you are imposter then' },
   { question: 'Which First english web series i watched  ', answer: '13 Reasons Why', rejectMessage: 'Told to my jaan multiple times, You are not my jaan' },
   { question: 'Which is my second favorite character in Harry Potter?', answer: 'Ron', rejectMessage: 'Avada Kedavara' },
-  { question: 'Which Firs Harry potter book i purchased ?', answer: 'Harry Potter and the cursed child', rejectMessage: 'You Filthy Muggle, Crucio!!' },
+  { question: 'Which First Harry potter book i purchased ?', answer: 'Harry Potter and the cursed child', rejectMessage: 'You Filthy Muggle, Crucio!!' },
   { question: 'In which month i gave you the first rose?', answer: 'December', rejectMessage: 'Not gonna talk to you, Bye' },
   { question: 'On which day i confessed my love?', answer: 'Thursday', rejectMessage: 'Bye' },
   { question: 'DO you Love Me?', answer: 'Yes', rejectMessage: 'Wrong Answer' },
