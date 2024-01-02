16d34191a7f7a37dc6e32fe800b4e4f7831b457b
iamthehalfblood <134286454+iamthehalfblood@users.noreply.github.com>
2024-01-02 12:23:44 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,11 +3,11 @@ const questions = [
   { question: 'What is your Name Beautiful?', answer: 'divya verma', rejectMessage: 'You are not my Babe, Sorry ' },
   { question: 'What is the first nickname i gave you on snapchat?', answer: 'DUMB Charade', rejectMessage: 'Do not Try. My babe already know the answer' },
   { question: 'In Which city did we met for the third time?', answer: 'Bareilly', rejectMessage: 'You called me in that city and you do not know, you are imposter then' },
-  { question: 'What is the square root of 144?', answer: '12', rejectMessage: 'Not quite right for the fourth question.' },
-  { question: 'Which element has the chemical symbol "O"?', answer: 'Oxygen', rejectMessage: 'Wrong! Try again for the fifth question.' },
-  { question: 'In which year did the Titanic sink?', answer: '1912', rejectMessage: 'Incorrect! The sixth question tests your historical knowledge.' },
-  { question: 'What is the capital of Japan?', answer: 'Tokyo', rejectMessage: 'Oops! The seventh question is about world capitals.' },
-  { question: 'Who painted the Mona Lisa?', answer: 'Da Vinci', rejectMessage: 'Incorrect! The eighth question is about art.' },
-  { question: 'What is the largest ocean on Earth?', answer: 'Pacific', rejectMessage: 'Wrong! The ninth question tests your geography.' },
-  { question: 'How many continents are there?', answer: '7', rejectMessage: 'Oops! The tenth question is about geography.' },
+  { question: 'Which First english web series i watched  ', answer: '13 Reasons Why', rejectMessage: 'Told to my jaan multiple times, You are not my jaan' },
+  { question: 'Which is my second favorite character in Harry Potter?', answer: 'Ron', rejectMessage: 'Avada Kedavara' },
+  { question: 'Which Firs Harry potter book i purchased ?', answer: 'Harry Potter and the cursed child', rejectMessage: 'You Filthy Muggle, Crucio!!' },
+  { question: 'In which month i gave you the first rose?', answer: 'December', rejectMessage: 'Not gonna talk to you, Bye' },
+  { question: 'On which day i confessed my love?', answer: 'Thursday', rejectMessage: 'Bye' },
+  { question: 'DO you Love Me?', answer: 'Yes', rejectMessage: 'Wrong Answer' },
+  
 ];
